b0e3fa7ad1069f7b36f6cf6958719baa03476d27
azmianshari <azmianshari123@gmail.com>
2024-11-22 11:26:27 +0700

    Menambahkan deskripsi

--- app/src/main/java/com/example/Navigasidengandata/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/Navigasidengandata/ui/view/screen/MahasiswaFormView.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,9 +15,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.Navigasidengandata.R
 
 @Composable
@@ -57,6 +62,27 @@ fun MahasiswaFormView(
                 ),
                 contentDescription = ""
             )
+
+            Spacer(modifier = Modifier.padding(start = 30.dp))
+
+
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+
+                Text(
+                    text = "Unggul dan Islami",
+                    color = Color.Red,
+                    fontWeight = FontWeight.Light
+                )
+
+
+            }
         }
     }
 }
