e2e9540657c867cd5698fc15b8b5abb6497f24de
azmianshari <azmianshari123@gmail.com>
2024-11-22 11:17:31 +0700

    Membuat state yang dapat diubah untuk menyimpan data input dari pengguna.

--- app/src/main/java/com/example/Navigasidengandata/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/Navigasidengandata/ui/view/screen/MahasiswaFormView.kt
@@ -1,9 +1,27 @@
 package com.example.Navigasidengandata.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun MahasiswaFormView(
     onSubmitButton: (MutableList<String>) -> Unit,
     onbackbuttonClicked: () -> Unit
-){}
+){
+    var nama by remember {
+        mutableStateOf("")
+    }
+
+
+    var nim by remember {
+        mutableStateOf("")
+    }
+
+
+    var email by remember {
+        mutableStateOf("")
+    }
+}
