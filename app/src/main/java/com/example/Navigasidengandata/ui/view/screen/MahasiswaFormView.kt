b065be9dfeb2c3517e5a4a3a25e9a1296671e37f
azmianshari <azmianshari123@gmail.com>
2024-11-22 11:14:56 +0700

    Membuat fungsi MahasiswaFormView

--- app/src/main/java/com/example/Navigasidengandata/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/Navigasidengandata/ui/view/screen/MahasiswaFormView.kt
@@ -1,2 +1,9 @@
 package com.example.Navigasidengandata.ui.view.screen
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MahasiswaFormView(
+    onSubmitButton: (MutableList<String>) -> Unit,
+    onbackbuttonClicked: () -> Unit
+){}
