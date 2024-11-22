bd6bd85629e694e1e2c0f54f78a8c0206304ac42
azmianshari <azmianshari123@gmail.com>
2024-11-22 11:03:53 +0700

    Membuat fungsi untuk MahasiswaApp

--- app/src/main/java/com/example/Navigasidengandata/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/Navigasidengandata/navigation/PengelolaHalaman.kt
@@ -1,2 +1,17 @@
 package com.example.Navigasidengandata.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.Navigasidengandata.ui.view.viewmodel.MahasiswaViewModel
+import com.example.Navigasidengandata.ui.view.viewmodel.RencanaStudyViewModel
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    RencanaStudyViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+)
