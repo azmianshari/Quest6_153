74990d915e1855e599d51b508f5d75c2c939dcb9
azmianshari <azmianshari123@gmail.com>
2024-11-22 11:25:40 +0700

    Menambahkan logo institusi

--- app/src/main/java/com/example/Navigasidengandata/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/Navigasidengandata/ui/view/screen/MahasiswaFormView.kt
@@ -1,5 +1,6 @@
 package com.example.Navigasidengandata.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.Navigasidengandata.R
 
@@ -49,7 +51,12 @@ fun MahasiswaFormView(
             .padding(40.dp),
             verticalAlignment = Alignment.CenterVertically,
         ){
-
+            Image(
+                painter = painterResource(
+                    id = R.drawable.umy
+                ),
+                contentDescription = ""
+            )
         }
     }
 }
