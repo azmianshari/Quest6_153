430f31ba0e519c47ce99eda7511a2b93844867e9
azmianshari <azmianshari123@gmail.com>
2024-11-22 11:24:55 +0700

    Membuat Row dengan ketentuan dan modifier

--- app/src/main/java/com/example/Navigasidengandata/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/Navigasidengandata/ui/view/screen/MahasiswaFormView.kt
@@ -2,14 +2,18 @@ package com.example.Navigasidengandata.ui.view.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.example.Navigasidengandata.R
 
 @Composable
@@ -41,6 +45,11 @@ fun MahasiswaFormView(
             )
         )
     ){
+        Row (modifier = Modifier
+            .padding(40.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ){
 
+        }
     }
 }
