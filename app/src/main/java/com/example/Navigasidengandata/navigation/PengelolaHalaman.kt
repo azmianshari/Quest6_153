1a128edb90b561e0e34cd2a7f114ffc2156ddbe3
azmianshari <azmianshari123@gmail.com>
2024-11-22 11:08:36 +0700

    Membuat State collection untuk menyimpan data state dari MahasiswaViewModel dan RencanaStudyViewModel

--- app/src/main/java/com/example/Navigasidengandata/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/Navigasidengandata/navigation/PengelolaHalaman.kt
@@ -1,9 +1,11 @@
 package com.example.Navigasidengandata.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.Navigasidengandata.ui.view.viewmodel.MahasiswaViewModel
 import com.example.Navigasidengandata.ui.view.viewmodel.RencanaStudyViewModel
@@ -17,4 +19,11 @@ fun MahasiswaApp(
 ){
     val mahasiswaUiState = mahasiswaViewModel.mahasiswaUiState.collectAsState().value
     val rencanaStudiUiState = RencanaStudyViewModel.krsStateUi.collectAsState().value
+
+
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = modifier.padding()
+    )
 }
